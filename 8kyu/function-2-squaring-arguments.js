ddd66ecb45ea3ced93a5aaaceb3d8e4b9db59378
rsimmons11 <ptgrd1980@gmail.com>
2023-06-27 15:30:30 -0700

    more challenges

---
+++ 8kyu/function-2-squaring-arguments.js
@@ -0,0 +1,20 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// My solutions
+
+function square(x) {
+  return Math.pow(x, 2)
+}
+
+
+
+
+const square = (n) => n * n;
+
+
+
+
+
+function square(x) {
+  return x ** 2;
+}
