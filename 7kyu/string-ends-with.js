0c35491436e8833436d6eba3f636321c8e6bb311
rsimmons11 <ptgrd1980@gmail.com>
2023-08-16 16:35:11 -0700

    more challenges

---
+++ 7kyu/string-ends-with.js
@@ -0,0 +1,17 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+
+// My Solution
+
+
+function solution(str, ending){
+  return str.endsWith(ending)
+}
+
+
+
+
+const solution = (str, ending) => str.endsWith(ending)
+
+
+
